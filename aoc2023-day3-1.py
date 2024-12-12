4bf8c0ff12a903f1f0160efd69d9723a57731e04
Peter Williams <peter.williams.97@gmail.com>
2024-12-12 15:52:42 +1100

    Rewrote to be possible to read top to bottom.

--- aoc2023-day3-1.py
+++ aoc2023-day3-1.py
@@ -4,13 +4,6 @@
 import re
 
 VERBOSE = False
-TEST = False
-PART_1 = False
-
-if TEST:
-    INPUT = "aoc2023-day3-input-test.txt"
-else:           
-    INPUT = "aoc2023-day3-input.txt"
 
 def matches_(regex, text):
     """
@@ -19,21 +12,11 @@ def matches_(regex, text):
     matches = regex.finditer(text)
     return [(m.start(), m.end(), m.group(0)) for m in matches]
 
-lines = open(INPUT).read().splitlines()
-
 # Regular expressions to find number
 RE_NUMBER = re.compile(r"(\d+)")
 # Regular expressions to find all symbols
 RE_SYMBOL = re.compile(r"[^.\d]")
 RE_STAR = re.compile(r"\*")
-
-if VERBOSE:
-    for regex in [RE_NUMBER, RE_SYMBOL]:
-        print("----------------------")
-        print(f"regex: {regex}")
-        for i, line in enumerate(lines):
-            matches = matches_(regex, line)
-            print(f"{i:4}: '{line}' {matches} ")
 
 def symbols_(regex, lines):
     """
@@ -52,25 +35,23 @@ def symbols_(regex, lines):
             symbols[i] |= set(range(start, end))
     return symbols
 
-
-   
-# Find all numbers in lines
-# `numbers` is a list of tuples (line number, start, end, value) for each number in `lines`.
-numbers = []
-for i, line in enumerate(lines):
-    matches = matches_(RE_NUMBER, line)
-    for m in matches:
-        start, end, v = m
-        numbers.append((i, start, end, int(v)))
+def numbers_(lines):
+    """Return a list of tuples (line number, start, end, value) for each number in `lines`.
+    """
+    numbers = []
+    for i, line in enumerate(lines):
+        matches = matches_(RE_NUMBER, line)
+        for m in matches:
+            start, end, v = m
+            numbers.append((i, start, end, int(v)))
+    return numbers
 
 def is_adjacent_(symbols, i, start, end):
     """
-    Return True if number at line `i`, `start` <= x < `end` is adjacent to a `symbols` element
+    Return True if the number at line `i`, `start` <= x < `end` is adjacent to a `symbols` element.
     """
-    y0 = max(0, i-1)
-    y1 = min(len(lines), i+2)
-    x0 = max(0, start-1)
-    x1 = min(len(line), end+1)
+    y0, y1 = i-1, i+2
+    x0, x1 = start-1, end+1
     for y in range(y0, y1):
         if y in symbols:
             for x in range(x0, x1):
@@ -80,13 +61,12 @@ def is_adjacent_(symbols, i, start, end):
 
 def adjacent_symbols_(symbols, i, start, end):
     """
-    Return True if number at line `i`, `start` <= x < `end` is adjacent to a `symbols` element
+    Returns (y, x) for all symbols[y][x] that are adjacent to the number at 
+    line `i`, `start` <= x < `end`.
     """
+    y0, y1 = i-1, i+2
+    x0, x1 = start-1, end+1
     adjacent = []
-    y0 = max(0, i-1)
-    y1 = min(len(lines), i+2)
-    x0 = max(0, start-1)
-    x1 = min(len(line), end+1)
     for y in range(y0, y1):
         if y in symbols:
             for x in range(x0, x1):
@@ -94,7 +74,11 @@ def adjacent_symbols_(symbols, i, start, end):
                     adjacent.append((y, x))
     return adjacent
 
-if PART_1:
+def part1(lines):
+    """
+    Find all numbers that are adjacent to symbols and print their sum.
+    """
+    numbers = numbers_(lines)
     symbols = symbols_(RE_SYMBOL, lines)
     if VERBOSE:
         print("----------------------")
@@ -109,16 +93,20 @@ if PART_1:
         print("----------------------")
         for  v in part_numbers:  
             print(f"{v}")
-    print(f"Sum of part numbers: {sum(part_numbers)}")
+    print(f"Part 1: Sum of part numbers: {sum(part_numbers)}")
 
-else:
+def part2(lines):
+    """
+    Find pairs of numbers that are adjacent to a "*" and print the sum of their products which is 
+    called the "gear ratio" in the problem description.
+    """
+    numbers = numbers_(lines)
     symbols = symbols_(RE_STAR, lines)
     symbols_neighbours = {}
     for i, start, end, v in numbers:
         adjacent_symbols = adjacent_symbols_(symbols, i, start, end) 
         if not adjacent_symbols:
             continue
-        # print(f"{v}: {adjacent_symbols}")
         for neighbour in adjacent_symbols:
             if neighbour not in symbols_neighbours:
                 symbols_neighbours[neighbour] = set()
@@ -130,5 +118,23 @@ else:
             print(f"{k}: {v}")
 
     gear_ratios = [v[0] * v[1] for v in symbols_neighbours.values()]
-    print(f"Gear ratio: {sum(gear_ratios)}")
+    print(f"Part 2: Gear ratio: {sum(gear_ratios)}")
 
+TEST = False
+if TEST:
+    INPUT = "aoc2023-day3-input-test.txt"
+else:           
+    INPUT = "aoc2023-day3-input.txt"
+
+lines = open(INPUT).read().splitlines()
+
+if VERBOSE:
+    for regex in [RE_NUMBER, RE_SYMBOL]:
+        print("----------------------")
+        print(f"regex: {regex}")
+        for i, line in enumerate(lines):
+            matches = matches_(regex, line)
+            print(f"{i:4}: '{line}' {matches} ")
+
+part1(lines)
+part2(lines)
