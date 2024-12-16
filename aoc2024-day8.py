84b5b89e0db260c229d5119fd1522ccc49a5e437
Peter Williams <peter.williams.97@gmail.com>
2024-12-16 21:53:46 +1100

    Solved https://adventofcode.com/2024/day/8 part 2

--- aoc2024-day8.py
+++ aoc2024-day8.py
@@ -24,80 +24,30 @@
 
     Part 2:
     ------
+    After updating your model, it turns out that an antinode occurs at any grid position exactly in
+    line with at least two antennas of the same frequency, regardless of distance. This means that
+    some of the new antinodes will occur at the position of each antenna (unless that antenna is
+    the only one of its frequency).
+
+    So, these three T-frequency antennas now create many antinodes:
+
+    T....#....
+    ...T......
+    .T....#...
+    .........#
+    ..#.......
+    ..........
+    ...#......
+    ..........
+    ....#.....
+    ..........
 
 """
 import time
 import numpy as np
-from scipy import ndimage
-from common import parse_args, read_rows, char_positions
+from common import parse_args, read_rows
 
-# Directions: (dy,dx) up, right, down, left
-DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-
-def is_edge(w, h, img, y, x):
-    "Return True if (y, x) is on the edge of the map."
-    if img[y, x] == 0:
-        return False
-    if y == 0 or y == h-1 or x == 0 or x == w-1:
-        return True
-    for dy, dx in DIRECTIONS:
-        ny, nx = y+dy, x+dx
-        if img[ny, nx] == 0:
-            return True
-    return False
-
-def edge_(img):
-    "Return the number of connected components that are on the edge of the map."
-    h,w = img.shape
-    edge = np.zeros((h, w), dtype=int)
-    for y in range(h):
-        for x in range(w):
-            if is_edge(w, h, img, y, x):
-                edge[y, x] = 1
-    edge += img
-    return edge
-
-def connected_components(rows):
-    w, h = len(rows[0]), len(rows)
-    img = np.zeros((h, w), dtype=int)
-    for y in range(h):
-        for x in range(w):
-            img[y, x] = 1 if rows[y][x] == "#" else 0
-
-    labeled, n = ndimage.label(img)
-    print(f"img={h}x{w}\n{img}")
-    print(f"labeled={labeled.shape}\n{labeled}")
-    print(f"n={n}")
-    for i in range(1, n+1):
-        print(f"Component {i}: {np.sum(labeled == i)}")
-        cpt = np.zeros((h, w), dtype=int)
-        cpt[labeled == i] = 1
-        print(f"{cpt}")
-        edge = edge_(cpt)
-        print(f"{edge}")
-
-def search(w, h, B, y0, x0, direction):
-    "Return True if the guard can escape the map of blockers `B`, False otherwise."
-    visited = {(y, x): set() for y in range(h) for x in range(w)}
-    y, x = y0, x0
-    dy, dx = DIRECTIONS[direction]
-    visited[(y, x)].add(direction)
-    n = 0
-    while True:
-        if (y+dy, x+dx) in B:
-            direction = (direction + 1) % 4
-            dy, dx = DIRECTIONS[direction]
-        else:
-            y, x = y+dy, x+dx
-            if not (0 <= x < w and 0 <= y < h):
-                return True
-            if direction in visited[(y, x)]:
-                return False
-            visited[(y, x)].add(direction)
-        n += 1
-        assert n < w * h * 1000, "Infinite loop"
-
-def antinodes_(img):
+def antinodes_(img, extend):
     "Return pairs coordinates of antinodes."
     w, h = img.shape
     points = set()
@@ -133,21 +83,51 @@ def antinodes_(img):
     for (y0, x0), (y1, x1) in sorted(pairs):
         dy = y1 - y0
         dx = x1 - x0
-        xlo = x0 - dx
-        xhi = x1 + dx
-        ylo = y0 - dy
-        yhi = y1 + dy
+        if dx == 0 or dy == 0:
+            continue
         elements = []
-        if add_antinode(ylo, xlo):
-           elements.append((ylo, xlo))
-        if add_antinode(yhi, xhi):
-            elements.append
+        ylo, xlo = y0, x0
+        while True:
+            xlo -= dx
+            ylo -= dy
+            if add_antinode(ylo, xlo):
+                elements.append((ylo, xlo))
+            else:
+                break
+            if not extend:
+                break
+        yhi, xhi = y1, x1
+        while True:
+            xhi += dx
+            yhi += dy
+            if add_antinode(yhi, xhi):
+                elements.append((yhi, xhi))
+            else:
+                break
+            if not extend:
+                break
         print(f"    {[y0,x0]}, {[y1,x1]} -> {elements}")
     return antinodes
 
 MARK = 9
 
-def part1(rows):
+ANSWER = """
+##....#....#
+.#.#....0...
+..#.#0....#.
+..##...0....
+....0....#..
+.#...#A....#
+...#..#.....
+#....#.#....
+..#.....A...
+....#....A..
+.#........#.
+...#......##
+"""
+ANSWER = ANSWER.strip().splitlines()
+
+def solve(rows, extend):
     "Solution to part 1. 14 for the test input."
     w, h = len(rows[0]), len(rows)
     img = np.zeros((h, w), dtype=int)
@@ -174,7 +154,7 @@ def part1(rows):
     all_antinodes = set()
     for k in vals:
         img_k = (img == k).astype(int)
-        antinodes = antinodes_(img_k)
+        antinodes = antinodes_(img_k, extend)
         valids = antinodes - exclusions[k]
         print(f"Antinodes {k}: {len(antinodes)}->{len(valids)} {antinodes} -> {valids}")
         # antinodes = valids
@@ -185,22 +165,32 @@ def part1(rows):
     for y, x in all_antinodes:
         img[y, x] = MARK
     print(f"img=\n{img}")
-    print(f"Part 1: {len(all_antinodes)}")
+
+    for i, row in enumerate(img):
+        n = sum(row!=0)
+        # m = len(ANSWER[i]) - ANSWER[i].count(".")
+        print(f"{i:2}: {n} {row}")
+    if extend:
+        return sum([sum(row!=0) for row in img])
+    return len(all_antinodes)
+
+def part1(rows):
+    "Solution to part 1. 14 for the test input."
+    n = solve(rows, False)
+    print(f"Part 1: {n}")
 
 def part2(rows):
-    "Solution to part 2. 6 for the test input."
-
-    print(f"Part 2: {len(obstructions)}")
+    "Solution to part 2. 34 for the test input."
+    n = solve(rows, True)
+    print(f"Part 2: {n}")
 
 args = parse_args("Advent of Code 2024 - Day 8", "aoc2024-day8-input-test.txt")
 rows = read_rows(args.input)
-# connected_components(rows)
-# exit(0)
 t0 = time.time()
 part1(rows)
 t1 = time.time() - t0
 t0 = time.time()
-# part2(rows)
+part2(rows)
 t2 = time.time() - t0
 print(f"Part 1: {t1:.1f} sec")
 print(f"Part 2: {t2:.1f} sec")
